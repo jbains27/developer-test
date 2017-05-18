57543447686bd8a438db72e266d271aa561b454f
Jasdeep Bains <jasdeep.bains@dealogic.com>
2017-05-18 17:09:26 -0400

    Add Book Viewing Test and Add Readme.txt with comments on the Code.

---
+++ OrangeBricks.Web.Tests/Controllers/Property/Commands/BookViewingCommandHandlerTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Entity;
+using NSubstitute;
+using NUnit.Framework;
+using OrangeBricks.Web.Controllers.Property.Commands;
+using OrangeBricks.Web.Models;
+using System.Linq;
+
+namespace OrangeBricks.Web.Tests.Controllers.Property.Commands
+{
+    [TestFixture]
+    public class BookViewingCommandHandlerTest
+    {
+        private BookViewingCommandHandler _handler;
+        private IOrangeBricksContext _context;
+        private IDbSet<Models.Property> _properties;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _context = Substitute.For<IOrangeBricksContext>();
+            _properties = Substitute.For<IDbSet<Models.Property>>();
+            _context.Properties.Returns(_properties);
+            _handler = new BookViewingCommandHandler(_context);
+        }
+
+        [Test]
+        public void AddViewingForProperty()
+        {
+            // Arrange
+            var command = new BookViewingCommand
+            {
+                PropertyId = 1
+            };
+
+            var property = new Models.Property
+            {
+                Description = "Test Property",       
+            };
+
+            _properties.Find(1).Returns(property);
+
+            // Act
+            _handler.Handle(command);
+
+            // Assert
+            _context.Properties.Received(1).Find(1);
+            _context.Received(1).SaveChanges();
+            Assert.True(property.Viewings.Any());
+        }
+    }
+}
---
+++ OrangeBricks.Web/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿Code Review
+
+The Code is well structured and follows separation of concerns by implementing different layers.
+Code Readability and reusability is good.
+
+Issues Found:
+
+1. User gets logged out on accessing unauthorized Page.
+2. Validation is not handled in Create Property Page.
+3. Error handling is missing ( -- Added Error Handling in Global.asax)
+
